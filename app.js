1860c2a88ff19d0c9df40f33893a2bafd71fd6e6
OikoumE <oikoume@gmail.com>
2021-08-15 22:12:54 +0200

    heroku test

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.get("/", (req, res) => {
 
 app.get("/twitch_auth", (req, res) => {
     result = twitch_api.validate_token(req)
+    console.log(result)
     res.status(200).json({ status: "Success!!!!", "data": result });
     // res.sendFile(__dirname + "/breakout/index.html");
 });
