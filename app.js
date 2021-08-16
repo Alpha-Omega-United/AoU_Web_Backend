9d77c85e5a8faaf55029f228b25501c0988e46ce
OikoumE <oikoume@gmail.com>
2021-08-17 00:42:27 +0200

    database preparations

--- app.js
+++ app.js
@@ -64,13 +64,16 @@ app.get("/twitch_auth", async (req, res) => {
 
 
 //* ------- MONGODB --------//
-app.put("/database", async (req, res) => {
+app.post("/database", async (req, res) => {
     console.log("-------------req-----------")
     console.log(req)
     console.log("-------------req.query-----------")
     console.log(req.query)
     console.log("-------------req.body-----------")
     console.log(req.body)
+    console.log(req.body.json())
+    console.log("-------------req.params-----------")
+    console.log(req.params)
     // result = await twitch_api.queryDb(req)
     // console.log(result)
     // res.status(200).json({ status: 200, "data": [result] });
