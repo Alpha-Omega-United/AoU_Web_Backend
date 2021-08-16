5b0aba7dd29e3c2d1bc35dcd2185b3655e166adb
OikoumE <oikoume@gmail.com>
2021-08-17 00:34:36 +0200

    database preparations

--- app.js
+++ app.js
@@ -73,7 +73,8 @@ app.put("/database", async (req, res) => {
     console.log(req.body)
     // result = await twitch_api.queryDb(req)
     // console.log(result)
-    res.status(200).json({ status: 200, "data": [result] });
+    // res.status(200).json({ status: 200, "data": [result] });
+    res.status(200).json({ status: 200, "data": ["result"] });
     // res.sendFile(__dirname + "/breakout/index.html");
 });
 
