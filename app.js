3d1b45573a099a8a052bd113f826afa08f57acb4
OikoumE <oikoume@gmail.com>
2021-08-19 13:26:48 +0200

    connecting edit/delete button to db

--- app.js
+++ app.js
@@ -49,10 +49,66 @@ app.post("/database", async (req, res) => {
         console.log("--------------------------------")
         const result = await twitch_api.queryDb(req.body)
         console.log(JSON.stringify(result))
-        console.log(result)
+        console.log(result[0].discord_id)
         res.status(200).json({ status: 200, "data": [JSON.stringify(result)] });
     }
 });
+
+
+
+// b=[
+//     {
+//     _id: new ObjectId("611a98d754868b1e9fde220a"),
+//     discord_name: 'itsOik#1508',
+//     discord_id: new Long("123600164433690625"),
+//     twitch_name: 'itsoik',
+//     twitch_id: 93645775,
+//     points: 0,
+//     isAdmin: true
+//     },
+//     {
+//     _id: new ObjectId("611c4e2f6ed80e33f46e9f90"),
+//     twitch_name: 'vivax3794',
+//     twitch_id: 180840730,
+//     discord_name: 'vivax#4105',
+//     discord_id: new Long("366331361583169537"),
+//     points: 0,
+//     isAdmin: true
+//     }
+//     ]
+
+
+// a=[
+// 	{
+// 		"_id": "611a98d754868b1e9fde220a",
+// 		"discord_name": "itsOik#1508",
+// 		"discord_id": {
+// 			"low": -683671551,
+// 			"high": 28777905,
+// 			"unsigned": false
+// 		},
+// 		"twitch_name": "itsoik",
+// 		"twitch_id": 93645775,
+// 		"points": 0,
+// 		"isAdmin": true
+// 	},
+// 	{
+// 		"_id": "611c4e2f6ed80e33f46e9f90",
+// 		"twitch_name": "vivax3794",
+// 		"twitch_id": 180840730,
+// 		"discord_name": "vivax#4105",
+// 		"discord_id": {
+// 			"low": -1254096895,
+// 			"high": 85293166,
+// 			"unsigned": false
+// 		},
+// 		"points": 0,
+// 		"isAdmin": true
+// 	}
+// ]
+
+
+
 
 //! ------------------------------------ LISTEN ------------------------------------ //
 httpServer.listen(process.env.PORT || httpPort, () => {
