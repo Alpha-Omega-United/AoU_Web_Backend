0503da185f85bb5a95d0d5e22cf3fd3409a9dd8e
OikoumE <oikoume@gmail.com>
2021-08-19 13:47:25 +0200

    connecting edit/delete button to db

--- app.js
+++ app.js
@@ -57,59 +57,6 @@ app.post("/database", async (req, res) => {
 
 
 
-// b=[
-//     {
-//     _id: new ObjectId("611a98d754868b1e9fde220a"),
-//     discord_name: 'itsOik#1508',
-//     discord_id: new Long("123600164433690625"),
-//     twitch_name: 'itsoik',
-//     twitch_id: 93645775,
-//     points: 0,
-//     isAdmin: true
-//     },
-//     {
-//     _id: new ObjectId("611c4e2f6ed80e33f46e9f90"),
-//     twitch_name: 'vivax3794',
-//     twitch_id: 180840730,
-//     discord_name: 'vivax#4105',
-//     discord_id: new Long("366331361583169537"),
-//     points: 0,
-//     isAdmin: true
-//     }
-//     ]
-
-
-// a=[
-// 	{
-// 		"_id": "611a98d754868b1e9fde220a",
-// 		"discord_name": "itsOik#1508",
-// 		"discord_id": {
-// 			"low": -683671551,
-// 			"high": 28777905,
-// 			"unsigned": false
-// 		},
-// 		"twitch_name": "itsoik",
-// 		"twitch_id": 93645775,
-// 		"points": 0,
-// 		"isAdmin": true
-// 	},
-// 	{
-// 		"_id": "611c4e2f6ed80e33f46e9f90",
-// 		"twitch_name": "vivax3794",
-// 		"twitch_id": 180840730,
-// 		"discord_name": "vivax#4105",
-// 		"discord_id": {
-// 			"low": -1254096895,
-// 			"high": 85293166,
-// 			"unsigned": false
-// 		},
-// 		"points": 0,
-// 		"isAdmin": true
-// 	}
-// ]
-
-
-
 
 //! ------------------------------------ LISTEN ------------------------------------ //
 httpServer.listen(process.env.PORT || httpPort, () => {
--- mongoDb/mongoDb.js
+++ mongoDb/mongoDb.js
@@ -19,12 +19,13 @@ async function queryOneDb(query) {
 	console.log("------------queryOneDb--------------")
 	console.log(query)
 	console.log("------------------------------------")
-	let result
+	let response = "",
+		result = "";
 	try {
 		await client.connect();
 		const database = client.db('aou_member_list');
 		const collection = database.collection('members');
-		result = await collection.findOne(query);
+		response = await collection.findOne(query);
 	} finally {
 		await client.close();
 		return result;
@@ -34,12 +35,13 @@ async function queryManyDb(query) {
 	console.log("------------queryManyDb--------------")
 	console.log(query)
 	console.log("-------------------------------------")
-	let result
+	let response = "",
+		result = "";
 	try {
 		await client.connect();
 		const database = client.db('aou_member_list');
 		const collection = database.collection('members');
-		result = await collection.findMany(query);
+		response = await collection.findMany(query);
 	} finally {
 		await client.close();
 		return result;
@@ -47,12 +49,13 @@ async function queryManyDb(query) {
 }
 async function queryGetAllDb() {
 	console.log("------------queryGetAllDb--------------")
-	let result
+	let response = "",
+		result = "";
 	try {
 		await client.connect();
 		const database = client.db('aou_member_list');
 		const collection = database.collection('members');
-		result = await collection.find().toArray();
+		response = await collection.find().toArray();
 	} finally {
 		await client.close();
 		return result;
@@ -63,12 +66,13 @@ async function addDb(data) {
 	console.log("------------addDb--------------")
 	console.log(data)
 	console.log("-------------------------------")
-	let result
+	let response = "",
+		result = "";
 	try {
 		await client.connect();
 		const database = client.db('aou_member_list');
 		const collection = database.collection('members');
-		result = await collection.insertOne(data)
+		response = await collection.insertOne(data)
 	} finally {
 		await client.close();
 		return result;
@@ -79,12 +83,13 @@ async function deleteDb(data) {
 	console.log("------------deleteDb--------------")
 	console.log(data)
 	console.log("----------------------------------")
-	let result
+	let response = "",
+		result = "";
 	try {
 		await client.connect();
 		const database = client.db('aou_member_list');
 		const collection = database.collection('members');
-		result = await collection.deleteOne(data)
+		response = await collection.deleteOne(data)
 	} finally {
 		await client.close();
 		return result;
@@ -94,15 +99,17 @@ async function deleteDb(data) {
 async function editDb(data) {
 	console.log("------------editDb--------------")
 	console.log(data)
-	data[whatever] = 
-	console.log("--------------------------------")
-	let result
+	data[whatever] =
+		console.log("--------------------------------")
+	let response = "",
+		result = "";
 	try {
 		await client.connect();
 		const database = client.db('aou_member_list');
 		const collection = database.collection('members');
-		result = await collection.updateOne({ twitch_id: data.twitch_id }, data)
-		result = await collection.updateMany
+		response = await collection.updateOne({ twitch_id: data.twitch_id }, data)
+		result = await parseDiscordID(response)
+		// response = await collection.updateMany
 	} finally {
 		await client.close();
 		return result;
@@ -110,4 +117,79 @@ async function editDb(data) {
 }
 
 
+
+async function parseDiscordID(memberArray) {
+	const tempArray = []
+	for (user of memberArray) {
+		const tempUser = {}
+		for (const [key, value] of Object.entries(user)) {
+			if (key == "discord_id") {
+				tempUser[key] = parseInt(value)
+			} else {
+				tempUser[key] = value
+			}
+		}
+		tempArray.push(tempUser)
+	}
+	return tempArray
+}
+
+
+
 module.exports = { queryOneDb, queryManyDb, queryGetAllDb, addDb, deleteDb, editDb }
+
+
+
+
+
+
+// b=[
+//     {
+//     _id: new ObjectId("611a98d754868b1e9fde220a"),
+//     discord_name: 'itsOik#1508',
+//     discord_id: new Long("123600164433690625"),
+//     twitch_name: 'itsoik',
+//     twitch_id: 93645775,
+//     points: 0,
+//     isAdmin: true
+//     },
+//     {
+//     _id: new ObjectId("611c4e2f6ed80e33f46e9f90"),
+//     twitch_name: 'vivax3794',
+//     twitch_id: 180840730,
+//     discord_name: 'vivax#4105',
+//     discord_id: new Long("366331361583169537"),
+//     points: 0,
+//     isAdmin: true
+//     }
+//     ]
+
+
+// a=[
+// 	{
+// 		"_id": "611a98d754868b1e9fde220a",
+// 		"discord_name": "itsOik#1508",
+// 		"discord_id": {
+// 			"low": -683671551,
+// 			"high": 28777905,
+// 			"unsigned": false
+// 		},
+// 		"twitch_name": "itsoik",
+// 		"twitch_id": 93645775,
+// 		"points": 0,
+// 		"isAdmin": true
+// 	},
+// 	{
+// 		"_id": "611c4e2f6ed80e33f46e9f90",
+// 		"twitch_name": "vivax3794",
+// 		"twitch_id": 180840730,
+// 		"discord_name": "vivax#4105",
+// 		"discord_id": {
+// 			"low": -1254096895,
+// 			"high": 85293166,
+// 			"unsigned": false
+// 		},
+// 		"points": 0,
+// 		"isAdmin": true
+// 	}
+// ]
